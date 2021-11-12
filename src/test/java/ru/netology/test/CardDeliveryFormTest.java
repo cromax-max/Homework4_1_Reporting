f18d43bf46315c0c33dd9faaa3a64b3691875f86
Sergey <tel.cromax@gmail.com>
2021-11-12 14:49:03 +1100

    add Allure reporting

--- src/test/java/ru/netology/test/CardDeliveryFormTest.java
+++ src/test/java/ru/netology/test/CardDeliveryFormTest.java
@@ -1,6 +1,9 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.CardDeliveryFormData;
@@ -14,6 +17,11 @@ import static com.codeborne.selenide.Selenide.*;
 public class CardDeliveryFormTest {
 
     CardDeliveryFormData data;
+
+    @BeforeAll
+    static void setUpReporting() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
     @BeforeEach
     void setUp() {
